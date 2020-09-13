cb1081317c9e9f2545b652c2ce479e813e234a60
Az <40351751+Mystos@users.noreply.github.com>
2020-09-13 19:45:10 +0200

    Remove Test on WorldInitialization

--- Project T Game/Assets/WorldInitialization.cs
+++ Project T Game/Assets/WorldInitialization.cs
@@ -22,39 +22,39 @@ public class WorldInitialization : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Pathfinder.Instance.grid.GetXY(vec, out int x, out int y);
-            Pathfinder.Instance.grid.GetXY(player.position, out int xx, out int yy);
-            List<Vector3> path = Pathfinder.Instance.FindPath(new Vector3(xx, yy, 0), new Vector3(x, y, 0));
-            if (path != null)
-            {
-                for (int i = 0; i < path.Count - 1; i++)
-                {
-                    Debug.DrawLine(path[i], path[i + 1], Color.green, 100f);
-                }
-            }
-            //Pathfinder.Instance.grid.GetXY(vec, out int xx, out int yy);
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        Pathfinder.Instance.grid.GetXY(vec, out int x, out int y);
+    //        Pathfinder.Instance.grid.GetXY(player.position, out int xx, out int yy);
+    //        List<Vector3> path = Pathfinder.Instance.FindPath(new Vector3(xx, yy, 0), new Vector3(x, y, 0));
+    //        if (path != null)
+    //        {
+    //            for (int i = 0; i < path.Count - 1; i++)
+    //            {
+    //                Debug.DrawLine(path[i], path[i + 1], Color.green, 100f);
+    //            }
+    //        }
+    //        //Pathfinder.Instance.grid.GetXY(vec, out int xx, out int yy);
 
-            //tilemap.SetTile(tilemap.WorldToCell(new Vector3(xx, yy,0)), tile);
+    //        //tilemap.SetTile(tilemap.WorldToCell(new Vector3(xx, yy,0)), tile);
 
-            foreach (PathNode pNode in Pathfinder.Instance.grid.gridArray)
-            {
-                Pathfinder.Instance.grid.GetXY(new Vector3(pNode.X, pNode.Y), out int xxx, out int yyy);
-                if(tilemap.HasTile(tilemap.WorldToCell(new Vector3(xxx, yyy, 0)))){
-                    tilemap.SetTile(tilemap.WorldToCell(new Vector3(xxx, yyy, 0)), tile);
-                }
-            }
-        }
-    }
+    //        //foreach (PathNode pNode in Pathfinder.Instance.grid.gridArray)
+    //        //{
+    //        //    Pathfinder.Instance.grid.GetXY(new Vector3(pNode.X, pNode.Y), out int xxx, out int yyy);
+    //        //    if(tilemap.HasTile(tilemap.WorldToCell(new Vector3(xxx, yyy, 0)))){
+    //        //        tilemap.SetTile(tilemap.WorldToCell(new Vector3(xxx, yyy, 0)), tile);
+    //        //    }
+    //        //}
+    //    }
+    //}
 
-    public static Vector3 GetMouseWorldPosition()
-    {
-        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        vec.z = 0f;
-        return vec;
-    }
+    //public static Vector3 GetMouseWorldPosition()
+    //{
+    //    Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    vec.z = 0f;
+    //    return vec;
+    //}
 }
